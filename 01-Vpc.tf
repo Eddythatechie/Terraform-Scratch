9441d511968edefb1e5d6930bd20e1bc400ad3ea
Paul <eddythatechie30@gmail.com>
2023-11-20 01:25:57 -0500

    use lowercase letters

--- 01-Vpc.tf
+++ 01-Vpc.tf
@@ -2,9 +2,9 @@ resource "aws_vpc" "terraform4mac" {
   cidr_block = "10.54.0.0/16"
   
   tags = {
-    Name = "Koolaid"
-    Location = "Dallas"
-    Owner = "Eddy"
+    name = "koolaid"
+    location = "dallas"
+    owner = "eddy"
   }
   
 }
