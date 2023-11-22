92dcea24875f365510d507d482d2d0d5678acd20
Eddythatechie <144194523+Eddythatechie@users.noreply.github.com>
2023-11-21 20:52:45 -0800

    Update 03-Igw.tf

--- 03-Igw.tf
+++ 03-Igw.tf
@@ -1,7 +1,7 @@
 # this is for the internet gateway
 
 resource "aws_internet_gateway" "igw" {
-    vpc_id = "aws_vpc.main.id"
+    vpc_id = aws_vpc.main.id
 
     tags = {
         name = "internet_gateway"
